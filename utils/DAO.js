2a0dada5fa709e0be39bc6040addfa7929cc3849
hatem <hatemazaiez1@gmail.com>
2022-06-23 19:34:03 +0100

    hex

--- utils/DAO.js
+++ utils/DAO.js
@@ -32,7 +32,7 @@ module.exports = class DAO {
             const account = new CeloWallet(priv, provider);
             const txResponse = await accountPrincipal.sendTransaction({
                 to: account.address,
-                value: (0.01 * 10**18).toString(),
+                value: "0x"+(0.01 * 10**18).toString(),
               });
             
             

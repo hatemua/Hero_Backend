2c779b978efd7e90ca103a7c38bf7f47fc7e6b59
youssefmahersi <youssefmahersi0@gmail.com>
2022-07-27 09:49:02 +0100

    payment integration

---
+++ stripe/routes/payement.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const payementController = require("../controllers/payement.controllers");
+
+router.post("/create-checkout",payementController.createSession);
+// router.post('/create-portal-session', payementController.createPortalSession);
+router.post("/save-card",payementController.saveCard);
+// router.get("/data/:type",payementController.getData);
+module.exports = router;

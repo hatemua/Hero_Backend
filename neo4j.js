57321bb468a6076cc399b186e7dfa9461f7a92a2
hatem <hatemazaiez1@gmail.com>
2022-08-31 15:51:37 +0100

    AAA

--- neo4j.js
+++ neo4j.js
@@ -2,7 +2,7 @@ const neo4j = require("neo4j-driver");
 require('dotenv').config()
 let driver;
 exports.initDriver=()=> {
-    console.log(process.env.USER);
+    console.log(process.env.USERR);
     // TODO: Create an instance of the driver here
     driver = neo4j.driver(
       process.env.DBURL,

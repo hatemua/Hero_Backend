d3e54d5ac514fad6ed0cc53a8ae570bafdc6421e
hatem <hatemazaiez1@gmail.com>
2022-09-07 16:04:51 +0100

    websocket

---
+++ websocket.js
@@ -0,0 +1,19 @@
+import { createServer } from 'https';
+import { readFileSync } from 'fs';
+import { WebSocketServer } from 'ws';
+
+const server = createServer({
+  cert: readFileSync('/path/to/cert.pem'),
+  key: readFileSync('/path/to/key.pem')
+});
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
+
+server.listen(8081);

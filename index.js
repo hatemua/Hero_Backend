b9962609e466fb09be5735a289be1f973b31e465
hatem <hatemazaiez1@gmail.com>
2022-09-08 16:59:16 +0100

    upload

--- index.js
+++ index.js
@@ -301,6 +301,67 @@ app.post("/CreateWallet", async (req, res) => {
   }
   
 });
+
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+
+      // Uploads is the Upload_folder_name
+      cb(null, "uploads")
+  },
+  filename: function (req, file, cb) {
+    cb(null,  Date.now()+ "-" +file.fieldname )
+  }
+})
+     
+// Define the maximum size for uploading
+// picture i.e. 1 MB. it is optional
+const maxSize = 100 * 1000 * 1000;
+  
+var upload = multer({ 
+  storage: storage,
+  limits: { fileSize: maxSize },
+  fileFilter: function (req, file, cb){
+  
+      // Set the filetypes, it is optional
+      var filetypes = /jpeg|jpg|png|avi|mp4|m4v/;
+      var mimetype = filetypes.test(file.mimetype);
+
+      var extname = filetypes.test(path.extname(
+                  file.originalname).toLowerCase());
+      
+      if (mimetype && extname) {
+          return cb(null, true);
+      }
+    
+      cb("Error: File upload only supports the "
+              + "following filetypes - " + filetypes);
+    } 
+
+// mypic is the name of file attribute
+}).single("myFile");   
+
+app.post("/uploadUpdatesFile",function (req, res, next) {
+        
+  // Error MiddleWare for multer file upload, so if any
+  // error occurs, the image would not be uploaded!
+  upload(req,res,function(err) {
+
+      if(err) {
+
+          // ERROR occurred (here it can be occurred due
+          // to uploading image of size greater than
+          // 1MB or uploading different file type)
+          res.send(err)
+      }
+      else {
+
+          // SUCCESS, image successfully uploaded
+          res.send("Success, Image uploaded!")
+      }
+  })
+})
+
 app.post("/CreateWalletMobelizer", async (req, res) => {
   // var web3 = new Web3(new Web3.providers.HttpProvider('https://polygon-rpc.com'));
   // A=web3.eth.accounts.create("87h0u74+-*/");

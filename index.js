149fb88cceda088739d5b0c989402f929f63e017
hatem <hatemazaiez1@gmail.com>
2022-09-24 00:39:15 +0100

    hh

--- index.js
+++ index.js
@@ -889,10 +889,11 @@ app.post("/CheckPassword", async (req, res) => {
   var session = driver.session({
     database: 'Hero'
   })
+  const decPassword =AESEncyption(numeroTel+"+-*/"+password,password);
   const result=await session
   .run('match (c:Customer {email:$Email,password:$Password})RETURN c', { 
     Email:numeroTel,
-   Password:AESDecryption(numeroTel+"+-*/"+password)
+   Password:AESDecryption(numeroTel+"+-*/"+password,decPassword)
     
   });
     

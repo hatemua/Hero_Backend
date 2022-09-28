d6221364a27fd1698a846b8548b9dae1014b4e8c
hatem <hatemazaiez1@gmail.com>
2022-09-28 20:51:37 +0100

    kk

--- index.js
+++ index.js
@@ -1022,17 +1022,16 @@ app.post("/HistoryTransactions", async (req, res) => {
   res.end(JSON.stringify(Tx));
   
 });
-/*const options = {
+const options = {
     key: fs.readFileSync('/opt/lampp/htdocs/HeroCoin/hegemony.donftify.digital/privkey2.pem'),
     cert: fs.readFileSync('/opt/lampp/htdocs/HeroCoin/hegemony.donftify.digital/fullchain2.pem')
   
-};*/
- app.listen(process.env.PORT || 8000, () => {
+};
+/* app.listen(process.env.PORT || 8000, () => {
    console.log("Serveur à l'écoute on ");
  });
+*/
 
 
-/*
 const server = https.createServer(options,app);
 server.listen(8080);
-*/

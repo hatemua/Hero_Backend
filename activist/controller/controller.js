87daf06bca6b418310c67ad34d9428255fa8af75
hatem <hatemazaiez1@gmail.com>
2022-10-12 23:06:03 +0100

    add circle to Mobilizer info

--- activist/controller/controller.js
+++ activist/controller/controller.js
@@ -73,7 +73,8 @@ exports.getActivistByAccID = async(req,res,next)=>{
         defaultAccessMode: neo4j.session.WRITE
 })
     var result = await session.run("match(a:Activist{accountId:$accountId})-[]-(c:Moments) return a.Name as name,a.accountId as username,a.address as address,a.description as description,a.Socials as Socials,a.Media as video,a.videoPoster as videoPoster,c.description as historicMoments",{accountId});
-    
+    var result1 = await session.run("match(a:Activist{accountId:$accountId})-[]-(c:Groupe) return c.Name ",{accountId});
+
     let Memories=[]
     result.records?.map(record => Memories.push(record._fields[7]) )
     let activist={}
@@ -92,6 +93,7 @@ exports.getActivistByAccID = async(req,res,next)=>{
         historicMoments: Memories,
         nextMoment:
           "Don’t miss out on the next one! Support this Circle and acces exclusive updates.",
+        circle : result1.records[0]._fields[0]
     
         };
         return res.status(200).json(activist)
